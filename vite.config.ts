51e859580e7abf164b184357d27efb22a45660ff
Christopher Snay <christopher.snay@gmail.com>
2026-07-19 19:39:43 -0400

    Implements manualChunks for smaller deploy chunks. Removes some unused dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,21 @@ export default defineConfig(({ mode }) => {
   return {
     base: mode === "production" ? "/minecraft-log-visualizer/" : "/",
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              if (id.includes("chart.js") || id.includes("react-chartjs-2")) {
+                return "chartjs";
+              }
+              if (id.includes("@mui") || id.includes("@emotion")) {
+                return "mui";
+              }
+            }
+          },
+        },
+      },
+    },
   };
 });
